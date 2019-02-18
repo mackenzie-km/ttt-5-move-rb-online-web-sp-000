3ef77fe4ae325c4a5b9d671defadc261c146b5d4
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-18 22:35:36 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,8 +6,20 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+index = 0
+
 def input_to_index
+  input = gets.strip
+  index = (input - 1)
 end
 
-def move
+def move(index)
+  x_or_o = gets.strip
+  if x_or_o == "x"
+    board[index] = "x"
+  elsif x_or_o == "o"
+    board[index] = "o"
+  else 
+    puts "Invalid! Choose X or O."
+     x_or_o = gets.strip
 end
